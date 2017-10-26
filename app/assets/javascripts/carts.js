bed4f0a557a8fa89331a8de3a4148a4ebf2ea906
Alexander Kalik <alexanderk1996@gmail.com>
2017-10-26 11:24:08 -0400

    trying to fix heroku errors

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -2,7 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-// window.onload=function(){
+window.onload=function(){
 //   document.getElementById('checkout').addEventListener("click", function(){
 //   alert("Confirm Checkout and Payment?");
 // });
