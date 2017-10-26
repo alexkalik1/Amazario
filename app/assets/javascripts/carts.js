87f2d3bb433ee145b4d1eb113f56d22aa5918283
Alexander Kalik <alexanderk1996@gmail.com>
2017-10-26 11:21:12 -0400

    makes checkout button redirect to home page (just for video)

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -2,10 +2,10 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-window.onload=function(){
-  document.getElementById('checkout').addEventListener("click", function(){
-  alert("Confirm Checkout and Payment?");
-});
+// window.onload=function(){
+//   document.getElementById('checkout').addEventListener("click", function(){
+//   alert("Confirm Checkout and Payment?");
+// });
 
 /* Set rates + misc */
 var taxRate = 0.05;
